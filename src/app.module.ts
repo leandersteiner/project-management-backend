4100e0438e25f2ca6fc1816398a2f34cdbf6255f
Leander Steiner <leandersteiner@yahoo.de>
2023-03-28 12:12:44 +0200

    generated all controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,10 +11,18 @@ import { TaskStateService } from './task-state/task-state.service';
 import { TaskService } from './task/task.service';
 import { SprintService } from './sprint/sprint.service';
 import { UserController } from './user/user.controller';
+import { BoardController } from './board/board.controller';
+import { BoardcolumnController } from './boardcolumn/boardcolumn.controller';
+import { BoardColumnController } from './board-column/board-column.controller';
+import { ProjectController } from './project/project.controller';
+import { SprintController } from './sprint/sprint.controller';
+import { TaskController } from './task/task.controller';
+import { TaskStateController } from './task-state/task-state.controller';
+import { TeamController } from './team/team.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController, BoardController, BoardcolumnController, BoardColumnController, ProjectController, SprintController, TaskController, TaskStateController, TeamController],
   providers: [AppService, PrismaService, UserService, TeamService, ProjectService, BoardService, BoardColumnService, TaskStateService, TaskService, SprintService],
 })
 export class AppModule {}
