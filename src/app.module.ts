9154f3a3c14672052852e769d905f8feac78e7ed
Leander Steiner <leandersteiner@yahoo.de>
2023-04-13 01:49:49 +0200

    mongo connection info is now inside .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TaskStateModule } from './task-state/task-state.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'process';
+
+
+const MONGO_URL = process.env.MONGO_URL || 'mongodb://mongo:mongo@db:27017';
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'mauwss2023';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongo:mongo@db:27017', { dbName: 'mauwss2023' }),
+    MongooseModule.forRoot(MONGO_URL, { dbName: MONGO_DB_NAME }),
     UserModule,
     TaskStateModule
   ]
