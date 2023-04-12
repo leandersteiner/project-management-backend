4db63f144b286713ba83ef80b50908aea52a03ae
Leander Steiner <leandersteiner@yahoo.de>
2023-04-13 00:27:33 +0200

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { TaskStateModule } from './task-state/task-state.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule]
+  imports: [
+    MongooseModule.forRoot('mongodb://mongo:mongo@db:27017', { dbName: 'mauwss2023' }),
+    UserModule,
+    TaskStateModule]
 })
-export class AppModule {}
+export class AppModule {
+}
