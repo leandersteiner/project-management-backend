5ef814c4f44a801b522480194a8b2e0cad7a40ff
Leander Steiner <leandersteiner@yahoo.de>
2023-04-12 19:14:25 +0200

    updated entrypoint and app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,48 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
-import { UserService } from './user/user.service';
-import { TeamService } from './team/team.service';
-import { ProjectService } from './project/project.service';
-import { BoardService } from './board/board.service';
-import { BoardColumnService } from './board-column/board-column.service';
-import { TaskStateService } from './task-state/task-state.service';
-import { TaskService } from './task/task.service';
-import { SprintService } from './sprint/sprint.service';
-import { UserController } from './user/user.controller';
-import { BoardController } from './board/board.controller';
-import { BoardColumnController } from './board-column/board-column.controller';
-import { ProjectController } from './project/project.controller';
-import { SprintController } from './sprint/sprint.controller';
-import { TaskController } from './task/task.controller';
-import { TaskStateController } from './task-state/task-state.controller';
-import { TeamController } from './team/team.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [
-    AppController,
-    UserController,
-    BoardController,
-    BoardColumnController,
-    ProjectController,
-    SprintController,
-    TaskController,
-    TaskStateController,
-    TeamController
-  ],
-  providers: [
-    AppService,
-    PrismaService,
-    UserService,
-    TeamService,
-    ProjectService,
-    BoardService,
-    BoardColumnService,
-    TaskStateService,
-    TaskService,
-    SprintService
-  ]
+  imports: [UserModule]
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-  await app.listen(3000);
+  await app.listen(8081);
 }
 bootstrap();
