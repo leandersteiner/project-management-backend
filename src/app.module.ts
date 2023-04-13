941d4215cb2b42e7eb168521e5871b27532d244b
Leander Steiner <leandersteiner@yahoo.de>
2023-04-13 18:21:37 +0200

    added feature modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { TaskStateModule } from './task-state/task-state.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TeamModule } from './team/team.module';
+import { TaskModule } from './task/task.module';
+import { SprintModule } from './sprint/sprint.module';
+import { ProjectModule } from './project/project.module';
+import { BoardColumnModule } from './board-column/board-column.module';
+import { BoardModule } from './board/board.module';
 import * as process from 'process';
 
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://mongo:mongo@db:27017';
@@ -11,7 +17,13 @@ const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'mauwss2023';
   imports: [
     MongooseModule.forRoot(MONGO_URL, { dbName: MONGO_DB_NAME }),
     UserModule,
-    TaskStateModule
+    TaskStateModule,
+    TeamModule,
+    TaskModule,
+    SprintModule,
+    ProjectModule,
+    BoardColumnModule,
+    BoardModule
   ]
 })
 export class AppModule {}
