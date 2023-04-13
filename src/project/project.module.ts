a26580a77fa894624e051ea4c8ac303e5297d45c
Leander Steiner <leandersteiner@yahoo.de>
2023-04-13 18:21:57 +0200

    fixed project module to export controller correctly

--- src/project/project.module.ts
+++ src/project/project.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Project, ProjectSchema } from './project.schema';
+import { ProjectController } from './project.controller';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Project.name, schema: ProjectSchema }])
   ],
-  controllers: [ProjectModule],
+  controllers: [ProjectController],
   providers: [ProjectService]
 })
 export class ProjectModule {}
