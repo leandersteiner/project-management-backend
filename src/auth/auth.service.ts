c4172e892f69b79e17e71f978bad3d39091a77dc
Leander Steiner <leandersteiner@yahoo.de>
2023-04-23 15:22:07 +0200

    added auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {}
+
+  async signIn(username: string, password: string): Promise<{ token: string }> {
+    const user = await this.userService.findByUsername(username);
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { id: user.id, username: user.username, sub: user.id };
+
+    return {
+      token: await this.jwtService.signAsync(payload)
+    };
+  }
+}
