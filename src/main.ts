9ba5abe0f9cf8c5137c2fd6ab136a366ac3923fb
Leander Steiner <leandersteiner@yahoo.de>
2023-04-23 15:22:36 +0200

    added mongoose plugin for ids

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose';
 
 async function bootstrap() {
+  mongoose.plugin((schema) => {
+    schema.set('toJSON', {
+      virtuals: true,
+      versionKey: false,
+      transform(doc, ret) {
+        delete ret._id;
+      }
+    });
+    schema.set('toObject', {
+      virtuals: true,
+      versionKey: false,
+      transform(doc, ret) {
+        delete ret._id;
+      }
+    });
+  });
   const app = await NestFactory.create(AppModule);
   await app.listen(8081);
 }
