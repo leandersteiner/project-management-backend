53b0e749c9e7cdaeda06b9c378cc3a1e777d2252
Leander Steiner <leandersteiner@yahoo.de>
2023-04-25 17:36:22 +0200

    added helmet swagger and cors

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import mongoose from 'mongoose';
+import helmet from 'helmet';
 import { useRequestLogging } from './common/request-logging';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   mongoose.plugin((schema) => {
@@ -22,7 +24,16 @@ async function bootstrap() {
     });
   });
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
   useRequestLogging(app);
+  const config = new DocumentBuilder()
+    .setTitle('Project Management')
+    .setDescription('Description')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(8081);
 }
 
