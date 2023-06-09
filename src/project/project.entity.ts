e9a34d84b332544d10df37e35170509907fe4e64
Leander Steiner <leandersteiner@yahoo.de>
2023-06-09 16:40:33 +0200

    removed unused imports in project entity

--- src/project/project.entity.ts
+++ src/project/project.entity.ts
@@ -2,12 +2,10 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  JoinColumn,
   JoinTable,
   ManyToMany,
   ManyToOne,
   OneToMany,
-  OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from 'typeorm';
