fec6ed2f940337954c5bec526d291bf0a9d52005
Leander Steiner <leandersteiner@yahoo.de>
2023-06-10 20:54:58 +0200

    updated task-comments

--- src/task/task-comment.entity.ts
+++ src/task/task-comment.entity.ts
@@ -3,11 +3,13 @@ import {
   CreateDateColumn,
   Entity,
   JoinColumn,
+  ManyToOne,
   OneToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from 'typeorm';
 import { User } from '../user/user.entity';
+import { Task } from './task.entity';
 
 @Entity()
 export class TaskComment {
@@ -23,6 +25,9 @@ export class TaskComment {
   @OneToOne(() => User)
   @JoinColumn()
   public creator: User;
+
+  @ManyToOne(() => Task, (task) => task.comments)
+  public task: Task;
 
   @CreateDateColumn({ type: 'timestamp' })
   public createdAt: Date;
