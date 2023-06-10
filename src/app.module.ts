64ac5067dc2118a53c8a52f835f809b449011c35
Leander Steiner <leandersteiner@yahoo.de>
2023-06-10 13:04:04 +0200

    added serve static to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,12 +11,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { getEnvPath } from './common/helper/env.helper';
 import { TypeOrmConfigService } from './common/typeorm/typeorm.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 const envFilePath = getEnvPath(`${__dirname}/common/envs`);
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    }),
     TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
     UserModule,
     TeamModule,
