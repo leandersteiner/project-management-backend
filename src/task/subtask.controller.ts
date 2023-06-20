c84e1b00e68e6677231ae01acb1fa1645edae61f
Leander Steiner <leandersteiner@yahoo.de>
2023-06-21 00:46:12 +0200

    added controller for subtasks

---
+++ src/task/subtask.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard';
+import { ReqUser } from '../common/helper/user.decorator';
+import { User } from '../user/user.entity';
+import { SubtaskService } from './subtask.service';
+import { CreateSubtaskDto } from './dto/create-subtask.dto';
+import { Subtask } from './subtask.entity';
+import { UpdateSubtaskDto } from './dto/update-subtask.dto';
+
+@UseGuards(AuthGuard)
+@Controller('tasks/:taskId/subtasks')
+export class TaskController {
+  constructor(private readonly subtaskService: SubtaskService) {}
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('/')
+  async create(
+    @ReqUser() user: User,
+    @Param('taskId') taskId: string,
+    @Body() createDto: CreateSubtaskDto
+  ): Promise<Subtask> {
+    return this.subtaskService.create(user, taskId, createDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch('/:subtaskId')
+  async update(
+    @Param('subtaskId') subtaskId: string,
+    @Body() updateDto: UpdateSubtaskDto
+  ): Promise<Subtask> {
+    return this.subtaskService.update(subtaskId, updateDto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':subtaskId')
+  async deleteSubtask(@Param('subtaskId') subtaskId: string): Promise<void> {
+    return this.subtaskService.delete(subtaskId);
+  }
+}
